e334a6f81b9d7bce509e7dccddc65343221de983
kaushikd1234 <kaushik.deo@gmail.com>
2024-03-28 14:18:01 +0530

    few changes

---
+++ src/main/java/com/javatechie/demo/flatmap/demo/FlatMapDemoMain.java
@@ -0,0 +1,36 @@
+package com.javatechie.demo.flatmap.demo;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class FlatMapDemoMain {
+
+    public static void main(String[] args) {
+
+        int[][] twoDimensionalArray = {{1, 2, 3},
+                {5, 6, 8},
+                {9, 10, 11}};
+
+        // convert that to array of integers
+
+        List<int[]> listOfIntegers = Arrays.asList(twoDimensionalArray);
+
+        List<String> integersList = listOfIntegers.stream().flatMap(element -> Arrays.stream(element).mapToObj(Integer::toString))
+                .collect(Collectors.toList());
+
+        System.out.println(integersList);
+
+        // convert the list oflist of Integers to List of Integers using flat Map
+
+        List<Integer> flattenedList = Arrays.asList(
+                        Arrays.asList(1, 2, 3),
+                        Arrays.asList(4, 5),
+                        Arrays.asList(6, 7, 8)).stream().flatMap(List::stream)
+                .collect(Collectors.toList());
+
+        System.out.println("flattened List is " + flattenedList);
+
+
+    }
+}
--- src/main/java/com/javatechie/stream/demo/FileReadFlatmapDemoMain.java
+++ src/main/java/com/javatechie/stream/demo/FileReadFlatmapDemoMain.java
@@ -2,23 +2,24 @@ package com.javatechie.stream.demo;
 
 import java.io.BufferedReader;
 import java.io.FileReader;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.Arrays;
 
 public class FileReadFlatmapDemoMain {
 
     public static void main(String[] args) {
 
+        String fileName = "C:/kd/myfile.txt";
+
         try {
-
-            BufferedReader reader = new BufferedReader(new FileReader("C:/file/sample.txt"));
-
+            BufferedReader reader = new BufferedReader(new FileReader(fileName));
             reader.lines().flatMap(line -> Arrays.stream(line.split("\\s+")))
                     .forEach(System.out::println);
 
         } catch (Exception exception) {
             exception.printStackTrace();
-
         }
-
     }
 }
